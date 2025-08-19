5f90d69829d27002a9a634a62f182eca909adca1
jyx0615 <jyx920615@gmail.com>
2025-08-19 19:19:56 +0800

    Imlement simple greeting and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Jackson User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Jackson.java
@@ -0,0 +1,13 @@
+public class Jackson {
+    public static void main(String[] args) {
+        String chatbotName = "Jackson";
+        // Greeting
+        System.out.println("--------------------------------------------");
+        System.out.printf("Hello! I'm %s.\n", chatbotName);
+        System.out.println("What can I do for you?");
+
+        // Exit
+        System.out.println("--------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
