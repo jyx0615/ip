60ce0d52e4bf65e9af316c234394f80fc34e46fd
jyx0615 <jyx920615@gmail.com>
2025-08-19 20:02:14 +0800

    Implement echo and exit methods

--- src/main/java/Jackson.java
+++ src/main/java/Jackson.java
@@ -5,9 +5,35 @@ public class Jackson {
         System.out.println("--------------------------------------------");
         System.out.printf("Hello! I'm %s.\n", chatbotName);
         System.out.println("What can I do for you?");
+        System.out.println("\n--------------------------------------------\n");
 
+
+        while(true) {
+            // Read user input
+            String userInput = System.console().readLine();
+            switch (userInput) {
+                case "bye":
+                    exit();
+                    return;
+                default:
+                    echo(userInput);
+                    break;
+            }
+        }
+    }
+
+    public static void echo(String message) {
+        // Echo the message
+        System.out.println("--------------------------------------------");
+        System.out.println(message);
+        System.out.println("\n--------------------------------------------\n");
+    }
+
+    public static void exit() {
         // Exit
         System.out.println("--------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\n--------------------------------------------\n");
+
     }
 }
