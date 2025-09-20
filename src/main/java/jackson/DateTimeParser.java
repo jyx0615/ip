6909332bf099bd02a2e98e139382287a783d28f7
jyx0615 <jyx920615@gmail.com>
2025-09-20 11:32:22 +0800

    Enhance DateTimeParser to support locale-specific date and time formatting

--- src/main/java/jackson/DateTimeParser.java
+++ src/main/java/jackson/DateTimeParser.java
@@ -3,8 +3,11 @@ package jackson;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DateTimeParser {
+    private static Locale sgLocale = Locale.forLanguageTag("en-SG");
+
     public static String formatDateAndTimeToFileString(LocalDate date, LocalTime time) {
         return date.toString() + (time != null ? " " + time.toString() : "");
     }
@@ -14,11 +17,19 @@ public class DateTimeParser {
     }
 
     private static String formatDate(LocalDate date) {
-        return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        return date.format(
+            DateTimeFormatter
+                .ofPattern("MMM d yyyy")
+                .withLocale(sgLocale)
+        );
     }
 
     private static String formatTime(LocalTime time) {
-        return time.format(DateTimeFormatter.ofPattern("hh:mm a"));
+        return time.format(
+            DateTimeFormatter
+                .ofPattern("hh:mm a")
+                .withLocale(sgLocale)
+        );
     }
 
     public static LocalDate parseDate(String dateStr) throws JacksonException {
